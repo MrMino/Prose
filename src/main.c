0d3f4d14b6d61e4f3b552f5f3f27f03118a13eef
Blazej Michalik <blazej.michalik@nokia.com>
2020-03-22 17:48:10 +0100

    Make prose listen on the created socket

--- src/main.c
+++ src/main.c
@@ -2,11 +2,16 @@
  * This file is part of Prose Proxy project.
  */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "fatal.h"
+
+#define LOCAL_PORT 12345
+#define MAX_LISTEN_BACKLOG 128
 
 int main() {
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -14,6 +19,19 @@ int main() {
         fatal_errno("Cannot create a socket", FATAL_SOCK_INIT_FAIL);
     }
 
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    server_addr.sin_port = htons(LOCAL_PORT);
+
+    if (bind(socketfd, (struct sockaddr*) &server_addr,
+                sizeof(struct sockaddr_in)) < 0) {
+        fatal_errno("Cannot bind", FATAL_SOCK_INIT_FAIL);
+    }
+
+    if (listen(socketfd, MAX_LISTEN_BACKLOG) < 0) {
+        fatal_errno("Cannot listen on server socket", FATAL_SOCK_INIT_FAIL);
+    }
 
     close(socketfd);
 }
