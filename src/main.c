82b54476d4baa69516b71a0533c729c264417fcb
Blazej Michalik <blazej.michalik@nokia.com>
2020-03-26 22:36:12 +0100

    Add recv_request_head() and timed_read()

--- src/main.c
+++ src/main.c
@@ -9,12 +9,90 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#include <sys/time.h>
+#include <errno.h>
+
 #include "fatal.h"
 
 #define LOCAL_PORT 12345
 #define MAX_LISTEN_BACKLOG 128
 
 #define BUFFER_LEN 1024
+
+/* Read at most bytes_max data available in the socket.
+ *
+ * This is a simple wrapper around select() -> recv().
+ * Received data is put into memory address pointed to by buffer argument.
+ *
+ * Returns the number of received bytes or -1 on error.
+ *
+ * If data is not received until specified timeout, sets errno to
+ * REQUEST_TIMED_OUT and return -1.
+ *
+ * NOTE: this implementation assumes Linux behavior of select() in regards to
+ * timeout. The timeval struct is therefore modified in-place after this call,
+ * to reflect the time left after select() returns.
+ */
+#define REQUEST_TIMED_OUT EWOULDBLOCK
+int timed_read(int sockfd, char *buffer, int bytes_max,
+               struct timeval *timeout) {
+    fd_set single_sock;
+    FD_ZERO(&single_sock);
+    FD_SET(sockfd, &single_sock);
+
+    int retval = select(sockfd + 1, &single_sock, NULL, NULL, timeout);
+    if (retval < 0) {
+        return -1;
+    } else if (retval == 0) {
+        errno = REQUEST_TIMED_OUT;
+        return -1;
+    } else {
+        return recv(sockfd, buffer, bytes_max, 0);
+    }
+}
+
+/* Read from socket until one of the following:
+ *  - "\r\n\r\n" (double CRLF) sequence is received
+ *  - received bytes_max bytes before double CRLF occured
+ *  - timeout is exceeded
+ *
+ * Received data is put into memory address pointed to by buffer argument.
+ * Buffer is NOT null terminated.
+ *
+ * NOTE: if additional bytes after double CRLF are received, they are put into
+ * the buffer with the rest of the data.
+ *
+ * Returns number of bytes received, if double CRLF is in the buffer after
+ * reading. Returns -1 on an error.
+ *
+ * Set errno to REQUEST_TOO_LONG if bytes_max is exceeded.
+ * Set errno to REQUEST_TIMED_OUT if timeout is exceeded.
+ */
+#define REQUEST_TOO_LONG ENOMEM
+int recv_request_head(int sockfd, char *buffer, int bytes_max,
+                      struct timeval *timeout) {
+    int byte_count = 0;
+    char *buffer_tail = buffer;
+    while (byte_count < bytes_max) {
+        int bytes_read = timed_read(sockfd, buffer_tail,
+                                    bytes_max - byte_count, timeout);
+        byte_count += bytes_read;
+
+        if (bytes_read < 0) {
+            return -1;
+        }
+        if (bytes_read == 0) {
+            return 0;
+        }
+        if (strstr(buffer, "\r\n\r\n") != NULL) {
+            return byte_count;
+        }
+        buffer_tail += bytes_read;
+    }
+
+    errno = REQUEST_TOO_LONG;
+    return -1;
+}
 
 /* TODO
  * 1. close() should be called after bind() or listen() fails.
